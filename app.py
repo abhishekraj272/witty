29e1769698d4b25fcece53fac9c0ba122b8bb31c
Abhishek Raj <abhishekraj272@gmail.com>
2020-07-26 14:44:03 +0530

    removed flaks test

--- app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, request
-app = Flask(__name__)
-
-@app.route('/upload', methods = ['POST'])
-def upload():
-    print(request.headers.__dict__)
-    return {'success':True}, 200, {'ContentType':'application/json'} 
